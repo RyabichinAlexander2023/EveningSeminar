21f5fdd0f8cb85b0310204f938621523bde6c0a5
Ryabichin Alexander <ryabichinsasha2013by@gmail.com>
2023-02-26 22:38:30 +0300

    seminar1.1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,52 @@
+﻿// int num1 = 25;
+// double num2 = 13.45;
+// string word = "Sasha";
+// char symbol = '%';
+// bool chek = num1 > num2;
+// Console.WriteLine ("my number is " + num1 + "and" + num2);
+// Console.WriteLine ($"my number are {num1}  and  {num2} ");
+
+//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+// Console.Write("Input a first number: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second number: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// int quad2 = number2 * number2;
+
+// if(number1 == quad2)  // точка с запятой не ставиться
+// {
+//     Console.WriteLine("Yes!");
+// }
+// else
+// {
+//     Console.WriteLine("No!");
+// }
+
+// Task 2. Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+/*
+Console.Write("Input a positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+    Console.Write(current + "");
+    current++; // current = current + 2 или current +=2  примеры
+}
+*/
+
+// 34215/10 3421
+// 34215/100 342
+// 34215/1000 34
+// 34215/10000 3
+
+// 34215%10     5
+// 34215%100    15 
+// 34215%1000   215
+// 34215%10000  4215
+
+// 28563 56     %1000/10  пример
---
+++ homework1/Program.cs
@@ -0,0 +1,76 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+//какое число большее, а какое меньшее.
+
+/*
+Console.Write("Input a first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+{
+    Console.WriteLine("Самое большое число " + number1);
+}
+else
+{
+    Console.WriteLine("Самое большое число " + number2);
+}
+*/
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*
+Console.WriteLine("Введите 3 числа:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int max = num1;
+
+            if (num2 > max)
+            {
+                max = num2;
+            }
+
+            if (num3 > max)
+            {
+                max = num3;
+            }
+
+            Console.WriteLine("Наибольшее из введённых чисел -> " + max);
+*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+//является ли число чётным (делится ли оно на два без остатка).
+
+/*
+Console.WriteLine("Введите число:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 1)
+            {
+                Console.WriteLine("Число " + num + " нечетное");
+            }
+            else
+            {
+                Console.WriteLine("Число " + num + " четное");
+            }
+*/
+
+//Задача 8: Напишите программу, которая на вход принимает число (N),
+//а на выходе показывает все чётные числа от 1 до N.
+
+/*
+int count = 1;
+            Console.WriteLine("Введите число:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Чётные числа от 1 до " + number);
+            while (count <= number)
+            {
+                if (count % 2 != 1)
+                {
+                    Console.Write(count + ". ");
+                    
+                }
+                count++;
+            }
+*/          
